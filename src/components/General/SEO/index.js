db701d77c9a7f09cac167bdabfc6194a1010ad21
viveralia <edgarv10@gmail.com>
2020-03-22 00:37:54 -0600

    Opengraph title bug fixed

--- src/components/General/SEO/index.js
+++ src/components/General/SEO/index.js
@@ -31,13 +31,19 @@ const SEO = ({ localTitle, localDesc }) => {
       <meta name="image" content={image} />
       {/* Twitter card */}
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={`${localTitle} | ${siteTitle}`} />
+      <meta
+        name="twitter:title"
+        content={localTitle ? `${localTitle} | ${siteTitle}` : siteTitle}
+      />
       <meta name="twitter:description" content={localDesc || siteDesc} />
       <meta name="twitter:image" content={`${siteUrl}${image}`} />
       {/* Facebook Cards */}
       <meta property="og:url" content={siteUrl} />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content={`${localTitle} | ${siteTitle}`} />
+      <meta
+        property="og:title"
+        content={localTitle ? `${localTitle} | ${siteTitle}` : siteTitle}
+      />
       <meta property="og:description" content={localDesc || siteDesc} />
       <meta property="og:image" content={`${siteUrl}${image}`} />
       <meta property="og:image:width" content="300" />
